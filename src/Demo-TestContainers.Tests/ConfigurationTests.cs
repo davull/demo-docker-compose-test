0e1a3951cafa4d7dfb354375af6c53ab18d7adb9
David Ullrich <david.ullrich@ullrich-software.de>
2024-04-27 12:53:47 +0200

    Add project

---
+++ src/Demo-TestContainers.Tests/ConfigurationTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using Xunit;
+
+namespace Demo_TestContainers.Tests;
+
+public class ConfigurationTests
+{
+    [Fact]
+    public void Should_Have_Configuration()
+    {
+        using var _ = new AssertionScope();
+        Configuration.DatabaseServer.Should().NotBeNullOrWhiteSpace();
+        Configuration.DatabasePort.Should().BePositive();
+        Configuration.DatabaseName.Should().NotBeNullOrWhiteSpace();
+        Configuration.DatabaseUser.Should().NotBeNullOrWhiteSpace();
+        Configuration.DatabasePassword.Should().NotBeNullOrWhiteSpace();
+    }
+}
---
+++ src/Demo-TestContainers.Tests/DatabaseFixture.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using Demo_TestContainers.Repositories;
+using Xunit;
+
+namespace Demo_TestContainers.Tests;
+
+public class DatabaseFixture : IAsyncLifetime
+{
+    public IDbConnection Connection { get; private set; } = null!;
+
+    public async Task InitializeAsync()
+    {
+        var databaseName = GetRandomTestDatabaseName();
+        Connection = await ConnectionProvider.GetOpenDatabaseConnection(connectionTimeout: 3);
+
+        await Database.Seed(Connection, databaseName);
+
+        SetEnvironmentVariables(databaseName);
+    }
+
+    public async Task DisposeAsync()
+    {
+        await Database.DeleteDatabase(Connection, Connection.Database);
+    }
+
+    public static string GetRandomTestDatabaseName()
+    {
+        return $"test_{Guid.NewGuid():N}";
+    }
+
+    private static void SetEnvironmentVariables(string databaseName)
+    {
+        Environment.SetEnvironmentVariable("DB_NAME", databaseName);
+    }
+}
